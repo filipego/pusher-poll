48cbb3daa5eed796848b99fbf021e5fa7474df14
Filipe Valente Gomes <Filipe@Filipes-iMac.fios-router.home>
2018-01-30 15:10:53 -0500

    added html part

--- routes/poll.js
+++ routes/poll.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+  appId: '465430',
+  key: 'd34835ee6fc766307396',
+  secret: 'e9ace7a6070455bca92e',
+  cluster: 'us2',
+  encrypted: true
+});
+
 router.get('/', (req, res) =>{
   res.send('POLL');
 })
+
+router.post('/', (req, res) =>{
+  pusher.trigger('os-poll', 'os-vote', {
+    points: 1,
+    os: req.body.os
+  });
+  return res.json({ success: true, message: 'Thanks you for voting.' })
+});
 
 module.exports = router;
